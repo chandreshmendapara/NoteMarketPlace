07ba8e6332635ac52955333644fc2bb92f17e94a
chandreshmendapara <chandreshmendapara336@gmail.com>
2021-02-21 11:27:50 +0530

    till 21/02/2021

---
+++ NoteMarketPlace/Controllers/AdminController.cs
@@ -0,0 +1,59 @@
+﻿using NoteMarketPlace.Models;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace NoteMarketPlace.Controllers
+{
+    public class AdminController : Controller
+    {
+        // GET: Admin
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public ActionResult UserQuery()
+        {
+
+            List<Contact> contacts = new List<Contact>();
+            
+            return View(contacts);
+        }
+
+        public ActionResult addNoteType()
+        {
+            return View();
+
+        }
+        public ActionResult addNoteCategory()
+        {
+            return View();
+
+        }
+        public ActionResult addCountry()
+        {
+            return View();
+
+        }
+        public ActionResult ManageNoteType()
+        {
+            return View();
+
+        }
+        public ActionResult ManageNoteCategory()
+        {
+            return View();
+
+        }
+        public ActionResult ManageCountry()
+        {
+            return View();
+
+        }
+
+    }
+}
