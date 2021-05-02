feb72e2bb1019275c8db455f53aab812f1064bd3
chandreshmendapara <chandreshmendapara336@gmail.com>
2021-05-02 08:51:18 +0530

    ...

--- mvc/NoteMarketPlace/NoteMarketPlace/Controllers/ChangePswdController.cs
+++ mvc/NoteMarketPlace/NoteMarketPlace/Controllers/ChangePswdController.cs
@@ -1,13 +1,23 @@
-﻿using System;
+﻿using NoteMarketPlace.Context;
+using System;
 using System.Collections.Generic;
+using System.Data.Entity.Validation;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
 
 namespace NoteMarketPlace.Controllers
 {
+    [Authorize]
     public class ChangePswdController : Controller
     {
+
+        private NotesMarketPlaceEntities _Context;
+        public ChangePswdController()
+        {
+            _Context = new NotesMarketPlaceEntities();
+
+        }
         // GET: ChangePswd
         public ActionResult Index()
         {
@@ -15,14 +25,39 @@ namespace NoteMarketPlace.Controllers
         }
 
         [HttpPost]
-        [ValidateAntiForgeryToken]
         public ActionResult Index(string oldPswd, string newPswd, string confirmPswd)
         {
+            if(newPswd.Equals(confirmPswd))
+            {
+                var user = _Context.tblUsers.Where(m => m.EmailID == User.Identity.Name).FirstOrDefault();
+                if (user.Password == oldPswd)
+                {
+                    user.Password = newPswd;
+                    try
+                    {
+                        
+                        _Context.SaveChanges();
+
+
+                    }
+                    catch (DbEntityValidationException ex)
+                    {
+                        string errorMessages = string.Join("; ", ex.EntityValidationErrors.SelectMany(x => x.ValidationErrors).Select(x => x.PropertyName + ": " + x.ErrorMessage));
+                        throw new DbEntityValidationException(errorMessages);
+                    }
+                }
+                else
+                {
+                    ViewBag.Error = "Incorrect Password";
+                    return View();
+                }
+
+            }
 
 
 
 
-            return View();
+            return RedirectToAction("logout","login");
         }
     }
 }
